e30005f62fbe117a92facacec4b74750a6d81e80
Joel Finger <joel.finger@semabit.ch>
2023-11-27 16:35:47 +0100

    Add IconButtons to Sidebar

--- src/components/src/CustomNavbar.tsx
+++ src/components/src/CustomNavbar.tsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import {FC} from "react";
+import {Divider, Flex} from "antd";
+import {IconButton} from "./customComponents/IconButton";
+import {TargetIcon} from "customComponents/assets/icons/target";
+import Ecolution from './customComponents/assets/images/ecolution';
 
 
 export const CustomNavbar: FC = () => {
+  const [activeButton, setActiveButton] = useState<number | null>(null);
 
-  return <div style={{position: 'absolute', display: 'flex', zIndex: 1, height: '100vh', width: 80, backgroundColor: 'blueviolet'}}>Hello</div>
+  const handleButtonClick = (buttonIndex: number) => {
+    setActiveButton(activeButton === buttonIndex ? null : buttonIndex);
+  }
+
+  return (
+    <Flex id={'sidebar'} style={{
+      position: 'absolute',
+      zIndex: 1,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      width: 80,
+      height: '100vh',
+      backgroundColor: 'blueviolet'
+    }}>
+      <Flex style={{ paddingTop: 20, paddingLeft: 5, paddingRight: 5}}>
+        {/*<img src={Ecolution} id={'ecolution'} />*/}
+      </Flex>
+      <Flex style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: 300 }}>
+        <div>Hello 2</div>
+      </Flex>
+      <Flex style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', paddingBottom: 40, alignContent: 'center', height: 250 }}>
+        <Divider />
+        <div>Hello 3</div>
+      </Flex>
+    </Flex>
+  )
 }
--- src/components/src/customComponents/IconButton.tsx
+++ src/components/src/customComponents/IconButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {FC, ReactNode} from "react";
+import {Button} from "antd";
 
 
 interface IconButtonProps {
@@ -9,9 +10,24 @@ interface IconButtonProps {
 }
 
 export const IconButton: FC<IconButtonProps> = ({icon, onClick, isActive}) => {
-  //const boxShadow = isActive ? 'inset 2px 2px 4px rgba(0, 0, 0, 0.9)' : 'none';
+  const boxShadow = isActive ? 'inset 2px 2px 4px rgba(0, 0, 0, 0.9)' : 'none';
 
   return (
-    <div>Hello</div>
+    <Button
+      type={"primary"}
+      shape={"round"}
+      className={isActive ? 'active' : ''}
+      icon={icon}
+      style={{
+        width: 60,
+        height: 60,
+        backgroundColor: isActive ? '#01544C' : '#018675',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        boxShadow: boxShadow
+      }}
+      onClick={onClick}
+    />
   )
 }
